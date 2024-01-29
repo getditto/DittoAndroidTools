b02a61ccf1ae1704adbbf02d844ffc3eb553333d
Anyer Castillo <anyer.castillo1@accesscfa.com>
2024-01-29 17:37:32 -0500

    UI layout for MissingPermissions, Wifi Status, Bluetooth Status. Introduces color and typography. BroadcastReceivers for wifi and bluetooth.

---
+++ DittoHealth/src/main/java/live/ditto/health/HealthUiActionType.kt
@@ -0,0 +1,8 @@
+package live.ditto.health
+
+sealed class HealthUiActionType {
+    object NoAction : HealthUiActionType()
+    object RequestPermissions : HealthUiActionType()
+    object EnableWifi : HealthUiActionType()
+    object EnableBluetooth : HealthUiActionType()
+}
