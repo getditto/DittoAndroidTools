9afd745fa112b71af884bcfd87b3b5fcd4accadf
Anyer Castillo <anyer.castillo1@accesscfa.com>
2024-03-05 23:35:57 -0500

    Adds FolderZip drawable

--- DittoDiskUsage/src/main/java/com/example/dittodiskusage/DiskUsageView.kt
+++ DittoDiskUsage/src/main/java/com/example/dittodiskusage/DiskUsageView.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FolderZip
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -80,7 +80,7 @@ private fun DiskUsageView(
                     onClick = { isExportDialogOpen = true }
                 ) {
                     Icon(
-                        painter = rememberVectorPainter(image = Icons.Default.FolderZip),
+                        painter = painterResource(R.drawable.folder_zip),
                         contentDescription = null,
                         modifier = Modifier.size(44.dp),
                         tint = Color.DarkGray
