1885be8a0302675f5c9b0082ff8100c74a83e998
Rajesh Ramsaroop <rajesh.ramsaroop@ditto.live>
2024-06-04 14:51:28 -0400

    remove navigation drawer

--- DittoToolsViewer/src/main/java/live/ditto/dittotoolsviewer/presentation/DittoToolsViewer.kt
+++ DittoToolsViewer/src/main/java/live/ditto/dittotoolsviewer/presentation/DittoToolsViewer.kt
@@ -3,37 +3,22 @@ package live.ditto.dittotoolsviewer.presentation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.launch
 import live.ditto.dittotoolsviewer.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DittoToolsViewer() {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
-    ToolsNavigationDrawer(drawerState = drawerState) {
-        DittoToolsViewerScaffold {
-            scope.launch {
-                drawerState.apply {
-                    if (isClosed) open() else close()
-                }
-            }
-        }
+    DittoToolsViewerScaffold {
+        //todo: show menu popup
     }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
