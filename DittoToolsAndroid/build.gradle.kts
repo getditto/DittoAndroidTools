546d67a4e30c03c103f1f464e15c7a16b1dd270b
Alexander <alexander.hsiao@ditto.live>
2025-04-24 15:57:58 -0400

    Remove ditto dependency strict versioning.

--- DittoToolsAndroid/build.gradle.kts
+++ DittoToolsAndroid/build.gradle.kts
@@ -32,11 +32,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.webkit)
 
-    implementation(libs.live.ditto.ditto) {
-        version {
-            strictly("[4.5.0,)")
-        }
-    }
+    implementation(libs.live.ditto.ditto)
 
     testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
