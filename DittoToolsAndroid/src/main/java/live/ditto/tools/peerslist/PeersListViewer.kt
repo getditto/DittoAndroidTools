1ebcd35a2fbc0caf8c2b8868648b69a5dc3b9f58
Alexander <alexander.hsiao@ditto.live>
2025-06-03 11:49:15 -0400

    Remove unused imports

--- DittoToolsAndroid/src/main/java/live/ditto/tools/peerslist/PeersListViewer.kt
+++ DittoToolsAndroid/src/main/java/live/ditto/tools/peerslist/PeersListViewer.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Card
