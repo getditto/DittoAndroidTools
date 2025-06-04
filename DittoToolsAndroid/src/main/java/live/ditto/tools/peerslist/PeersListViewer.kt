7fa6568c918c44d3d62ac29e3972e3d00c16aaad
Alexander <alexander.hsiao@ditto.live>
2025-06-04 16:41:39 -0400

    Remove Scaffold and update arguments for PeerListView

--- DittoToolsAndroid/src/main/java/live/ditto/tools/peerslist/PeersListViewer.kt
+++ DittoToolsAndroid/src/main/java/live/ditto/tools/peerslist/PeersListViewer.kt
@@ -1,9 +1,10 @@
 package live.ditto.tools.peerslist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,13 +20,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -44,53 +45,56 @@ fun PeersListViewer(
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
-            Surface(tonalElevation = 4.dp) {
-                TopAppBar(
-                    title = {
-                        Text(stringResource(R.string.peers_list_title))
-                    },
-                    actions = {
-                        IconButton(onClick = {
-                            viewModel.togglePause()
-                        }) {
-                            if (uiState.isPaused) {
-                                Icon(
-                                    imageVector = Icons.Default.PlayArrow,
-                                    contentDescription = null,
-                                    modifier = Modifier.size(24.dp)
-                                )
-                            } else {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_pause_circle),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(24.dp)
-                                )
-                            }
-                        }
-                    }
-                )
+    Column {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primary),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                stringResource(R.string.peers_list_title),
+                style = MaterialTheme.typography.titleLarge,
+                modifier = modifier
+                    .padding(16.dp),
+                color = Color.White
+            )
+
+            IconButton(onClick = {
+                viewModel.togglePause()
+            }) {
+                if (uiState.isPaused) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        modifier = modifier.size(24.dp),
+                        tint = Color.White
+                    )
+                } else {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_pause_circle),
+                        contentDescription = null,
+                        modifier = modifier.size(24.dp),
+                        tint = Color.White
+                    )
+                }
             }
         }
-    ) { innerPadding ->
-        PeerListView(uiState.localPeer, uiState.remotePeers, padding = innerPadding)
+
+        PeerListView(uiState)
     }
 }
 
 @Composable
 private fun PeerListView(
-    localPeer: DittoPeer,
-    remotePeers: List<DittoPeer>,
-    padding: PaddingValues
+    state: PeerListUiState
 ) {
     Surface {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(10.dp),
-            contentPadding = padding,
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             item {
@@ -100,7 +104,7 @@ private fun PeerListView(
                 )
             }
             item {
-                PeerView(localPeer)
+                PeerView(state.localPeer)
             }
 
             item {
@@ -110,7 +114,7 @@ private fun PeerListView(
                 )
             }
 
-            items(remotePeers, key = { it.peerKeyString }) { peer ->
+            items(state.remotePeers, key = { it.peerKeyString }) { peer ->
                 // Remote peer
                 PeerView(peer)
             }
